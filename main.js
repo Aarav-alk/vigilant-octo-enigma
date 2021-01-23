0a2cac583104a3311c3b4dc516d37c93e65688e9
Aarav-alk <74759331+Aarav-alk@users.noreply.github.com>
2021-01-23 16:03:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Canvas.addEventListener("mousedown",my_mousedown)
+
+
+function my_mousedown(e)
+{
+
+    color = document.getElementById("color").Value;
+    width_of_line = document.getElementById("width_of_line").value;
+    radius = docment.getElementById(" radius").Value;
+    MouseEvent="mouseEvent";
+}
+function my_mousemove(e)
+{
+current_position_of_mouse_x = e.clienx
+current_position_of_mouse_y = e.clieny 
+
+if (MouseEvent == "mosesedown")
+console.log("current position")
+}
